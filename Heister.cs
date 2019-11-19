e49e54769a23c48a3b95b92f55287d31c3855774
unknown <nwessel429@gmail.com>
2019-11-19 10:15:18 -0600

    added teammember

---
+++ Heister.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace heist
+{
+    public class Heister
+    {
+        public string Name { get; set; }
+
+        public int SkillLevel { get; set; }
+        public decimal CourageFactor { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine();
+
+            Heister teamMember;
+            Console.WriteLine("What is the team members name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is the team members skill level?");
+            string skillLevelString = Console.ReadLine();
+            int skillLevel;
+
+            try
+            {
+                skillLevel = int.Parse(skillLevelString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{skillLevelString} is not a valid skill level. Using a default value of 10");
+                skillLevel = 10;
+            }
+
+            Console.WriteLine("Waht is your team members courage factor?");
+            string courageFactorString = Console.ReadLine();
+            decimal courageFactor;
+
+            try
+            {
+                courageFactor = decimal.Parse(courageFactorString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{courageFactorString} is not a valid courage factor. Using a default value of 1.0");
+                courageFactor = 1.0M;
+            }
+
+            teamMember = new Heister()
+            {
+                Name = name,
+                SkillLevel = skillLevel,
+                CourageFactor = courageFactor
+            };
+
+            Console.WriteLine($"Name: {teamMember.Name}");
+            Console.WriteLine($"Skill Level: {teamMember.SkillLevel}");
+            Console.WriteLine($"Courage Factor: {teamMember.CourageFactor}");
+        }
+    }
+}
